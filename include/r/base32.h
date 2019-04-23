224c0fd8286e20dfb35ad0d851f81e249d3e6693
Gustav Behm <me@rootmos.io>
2019-04-23 17:27:57 +0200

    Add a BASE32_ENCODED_LENGTH macro

--- include/r/base32.h
+++ include/r/base32.h
@@ -2,4 +2,6 @@
 
 // https://tools.ietf.org/html/rfc4648
 
+#define BASE32_ENCODED_LENGTH(BIN_LEN) ((((BIN_LEN)/5)+((BIN_LEN)%5!=0))*8+1)
+
 size_t base32_encode(char* out, const void* data, size_t l);
--- tests/base32.c
+++ tests/base32.c
@@ -8,42 +8,49 @@ void base32_tests(void)
 {
     TEST(base32_encode_empty, {
          char enc[1];
+         assert(BASE32_ENCODED_LENGTH(0) == sizeof(enc));
          assert(base32_encode(enc, NULL, 0) == 1);
          assert(strcmp(enc, "") == 0);
     });
 
     TEST(base32_encode_f, {
          char enc[8+1];
+         assert(BASE32_ENCODED_LENGTH(1) == sizeof(enc));
          assert(base32_encode(enc, "f", 1) == sizeof(enc));
          assert(strcmp(enc, "MY======") == 0);
     });
 
     TEST(base32_encode_fo, {
          char enc[8+1];
+         assert(BASE32_ENCODED_LENGTH(2) == sizeof(enc));
          assert(base32_encode(enc, "fo", 2) == sizeof(enc));
          assert(strcmp(enc, "MZXQ====") == 0);
     });
 
     TEST(base32_encode_foo, {
          char enc[8+1];
+         assert(BASE32_ENCODED_LENGTH(3) == sizeof(enc));
          assert(base32_encode(enc, "foo", 3) == sizeof(enc));
          assert(strcmp(enc, "MZXW6===") == 0);
     });
 
     TEST(base32_encode_foob, {
          char enc[8+1];
+         assert(BASE32_ENCODED_LENGTH(4) == sizeof(enc));
          assert(base32_encode(enc, "foob", 4) == sizeof(enc));
          assert(strcmp(enc, "MZXW6YQ=") == 0);
     });
 
     TEST(base32_encode_fooba, {
          char enc[8+1];
+         assert(BASE32_ENCODED_LENGTH(5) == sizeof(enc));
          assert(base32_encode(enc, "fooba", 5) == sizeof(enc));
          assert(strcmp(enc, "MZXW6YTB") == 0);
     });
 
     TEST(base32_encode_foobar, {
          char enc[16+1];
+         assert(BASE32_ENCODED_LENGTH(6) == sizeof(enc));
          assert(base32_encode(enc, "foobar", 6) == sizeof(enc));
          assert(strcmp(enc, "MZXW6YTBOI======") == 0);
     });
