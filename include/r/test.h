2cb10d5dc9fdb25fa264aa52c8809d054c2cf469
Gustav Behm <me@rootmos.io>
2020-01-17 12:44:27 +0100

    Correct signal.h include

--- include/r/test.h
+++ include/r/test.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <sys/signal.h>
+#include <signal.h>
 
 #define TEST(f, body) { void f(void) { body } if(test_runner(#f, f, -1, 0) != 0) exit(1); } while(0)
 #define TEST_ABORT(f, body) { void f(void) { body } if(test_runner(#f, f, SIGABRT, -1) != 0) exit(1); } while(0)
