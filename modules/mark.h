91257bd5d9505103c96b54204c82a16e70e403da
Gustav Behm <me@rootmos.io>
2023-12-02 12:43:44 +0100

    Macroify the mark module

--- modules/mark.h
+++ modules/mark.h
@@ -2,11 +2,12 @@
 
 #include <stddef.h>
 
-struct mark* mark_init(const char* const what,
-                       const double factor,
-                       const char* const unit,
-                       const size_t check_period);
-void mark_free(struct mark* const m);
+struct mark* LIBR(mark_init)(
+    const char* const what,
+    const double factor,
+    const char* const unit,
+    const size_t check_period);
+void LIBR(mark_free)(struct mark* const m);
 
-void mark_tick(struct mark* const m);
-void mark_set(struct mark* const m);
+void LIBR(mark_tick)(struct mark* const m);
+void LIBR(mark_set)(struct mark* const m);
