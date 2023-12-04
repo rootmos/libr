d2b091e789ca256fddea6c63d8973ad224622475
Gustav Behm <me@rootmos.io>
2023-12-04 12:50:50 +0100

    Macroify the uv module
    [skip ci]

--- modules/uv.h
+++ modules/uv.h
@@ -3,9 +3,10 @@
 #ifdef UV_VERSION_MAJOR
 #define CHECK_UV(err, format, ...) do { \
     if(err != 0) { \
-        r_failwith(__extension__ __FUNCTION__, __extension__ __FILE__, \
-                   __extension__ __LINE__, 0, \
-                   format ": %s\n", ##__VA_ARGS__, uv_err_name(err)); \
+        LIBR(failwith0)( \
+            __extension__ __FUNCTION__, __extension__ __FILE__, \
+            __extension__ __LINE__, 0, \
+            format ": %s\n", ##__VA_ARGS__, uv_err_name(err)); \
     } \
 } while(0)
 #endif
